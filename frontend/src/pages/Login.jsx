7e6eeae61bfddf5efcd1dadbbb599480f67812bc
ogrit0 <nochujeon66@gmail.com>
2026-02-05 12:33:15 -0500

    iopap

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -7,13 +7,8 @@ const Login = () => {
       <form>
         <h2><center>Bienvenidos</center></h2>
         <div className="form-group">
-<<<<<<< HEAD
-          <label htmlFor="email">Correo</label>
-          <input type="email" id="email" name="email" placeholder="Correo papoi" required />
-=======
           <label htmlFor="email">Correo</label> 
           <input type="email" id="email" name="email" placeholder="Correo electrónico" required />
->>>>>>> 82b69cb6f8e00730b2e7ca2d6aac7d501b53d008
         </div>
         <div className="form-group">
           <label htmlFor="password">Contraseña</label>
